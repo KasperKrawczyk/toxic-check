fe8229f116820b66fd281e1dafaae24a10184747
KasperKrawczyk <kasper.krawczyk@gmail.com>
2023-03-18 17:06:18 +0000

    ETL wip

--- SVM.py
+++ SVM.py
@@ -3,14 +3,36 @@ import numpy as np
 
 
 class SVM:
-    def __init__(self, dataframe: pd.DataFrame, learn_rate=0.001, num_epochs=10000, c=0.1, train_ratio=0.8):
+    def __init__(self, dataframe: pd.DataFrame, class_col_name: str, learn_rate=0.001, num_epochs=10000, c=0.1, train_ratio=0.8):
         self.dataframe = dataframe
-        self.lear_rate = learn_rate
+        self.class_col_name = class_col_name
+        self.learn_rate = learn_rate
         self.num_epochs = num_epochs
         self.c = c
         self.num_samples, self.dimensions = dataframe.shape
         self.train_ratio = train_ratio
         self.w = np.zeros(self.dimensions)
+        self.bias = 0
         self.train_set = dataframe[:int(self.num_samples * train_ratio)]
         self.test_set = dataframe[int(self.num_samples - self.num_samples * train_ratio):]
+
+    def predict(self, x_i: pd.Series):
+        return np.sign(np.dot(x_i, self.w) - self.bias)
+
+    def train_predict(self, x_i_class: int, x_i: pd.Series):
+        return x_i_class * (np.dot(x_i, self.w) - self.bias) >= 1
+
+    def fit(self):
+        samples_classes = np.where(self.train_set[self.class_col_name] == 0, -1, 1)
+
+        for epoch in range(self.num_epochs):
+            for index, x_i in enumerate(self.train_set):
+                is_correctly_predicted = self.train_predict(samples_classes[index], x_i)
+
+                if is_correctly_predicted:
+                    self.w -= self.learn_rate * (0.5 * self.c * self.w)
+                else:
+                    self.w -= self.learn_rate * (0.5 * self.c * self.w - np.dot(x_i, samples_classes[index]))
+                    self.bias -= self.learn_rate * samples_classes[index]
+
 
--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import numpy as np
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
+from collections import Counter
 import string
 import winsound
 
 
-def process_text(raw_text, stopwords_set, porter_stemmer):
+def process_sample_text(raw_text: str, stopwords_set: set, porter_stemmer: PorterStemmer, vocab_counter: Counter):
     # split
     tokens = word_tokenize(raw_text)
     # to lower case, strip new line feeds, carriage returns, and non-alpha
@@ -19,34 +20,51 @@ def process_text(raw_text, stopwords_set, porter_stemmer):
     words_non_stopwords = [word[:100] for word in words_no_punct if word not in stopwords_set]
     # extract stems
     stems = [porter_stemmer.stem(word) for word in words_non_stopwords]
-    # print(stems[:20])
+    vocab_counter.update(stems)
     return stems
 
+def filter_min_occurrence_stems(stems: list, min_occurrence: int, vocab_counter: Counter):
+    return [stem for stem in stems if vocab_counter.get(stem) >= min_occurrence]
 
-stopwords_set = set(stopwords.words('english'))
-porter_stemmer = PorterStemmer()
+def save_vocabulary(vocabulary_counter: Counter, vocabulary_file_path: str):
+    with open(vocabulary_file_path, 'w') as vocab_file:
+        for stem, count in vocabulary_counter.items(): vocab_file.write(f'{stem},{count}\n')
 
-print(stopwords.words('english'))
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_colwidth', None)
+def process_dataset(dataset_file_path: str):
+    # covert to set the stopwords to remove from tokens
+    stopwords_set = set(stopwords.words('english'))
+    vocab_counter = Counter()
+    porter_stemmer = PorterStemmer()
 
-df = pd.read_csv('data/train.csv')
+    df = pd.read_csv(dataset_file_path)
 
-print(df.columns)
-print(df.iloc[:, 2:])
-df['profanity'] = np.where(df.iloc[:, 2:].sum(axis=1) > 0, 1, 0)
-print(df.head(10))
-winsound.Beep(560, 1000)
-df['tokens'] = df.apply(lambda x: process_text(x['comment_text'], stopwords_set, porter_stemmer), axis=1)
-tokens_col = df.pop('tokens')
-df.insert(2, 'tokens', tokens_col)
+    # a sample is classified as 'profanity' if any of the other classes is non-zero
+    df['profanity'] = np.where(df.iloc[:, 2:].sum(axis=1) > 0, 1, 0)
+    winsound.Beep(560, 1000)
+    # extract tokens
+    df['tokens'] = df.apply(lambda x: process_sample_text(x['comment_text'], stopwords_set, porter_stemmer, vocab_counter), axis=1)
+    # filter out words that occur fewer than two times in the vocabulary
+    df['tokens'] = df.apply(lambda x: filter_min_occurrence_stems(x['tokens'], 3, vocab_counter))
+    print(vocab_counter.most_common(50))
+    print("vocabulary list length = {:d}".format(len(vocab_counter)))
+    # move the tokens column 2 (after the raw text)
+    tokens_col = df.pop('tokens')
+    df.insert(2, 'tokens', tokens_col)
 
-winsound.Beep(440, 1000)
+    winsound.Beep(440, 1000)
+    # save
+    df.to_csv('data/clean.csv')
+    save_vocabulary(vocab_counter, 'data/vocabulary.txt')
+    winsound.Beep(340, 5000)
 
-df.to_csv('data/clean.csv')
+def vectorise(clean_dataset_file_path: str):
+    df = pd.read_csv(clean_dataset_file_path)
+    vocabulary_set = set()
 
-winsound.Beep(340, 5000)
+
+def __main__():
+
 
 
 
